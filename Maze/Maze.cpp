a124fa73ad4d4d08d2aba7fb3591bc98ececf135
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2022-03-14 16:37:00 +0200

    Update Maze.cpp

--- Maze/Maze.cpp
+++ Maze/Maze.cpp
@@ -1,111 +1,114 @@
 #include <iostream>
-#include <ctime>
 #include <windows.h>
 #include <conio.h>
 using namespace std;
 
-#pragma warning (disable:4996) 
-
-// enumeration (перечисление - набор целочисленных констант)
-// MazeObjects - пользовательский (кастомизированный) тип данных
-enum MazeObjects { HALL, WALL, COIN, ENEMY, BORDER };
-enum Colors { DARKGREEN = 2, YELLOW = 14, RED = 12, BLUE = 9, WHITE = 15, DARKYELLOW = 6, DARKRED = 4 };
-enum KeyCodes { ENTER = 13, ESCAPE = 27, SPACE = 32, LEFT = 75, RIGHT = 77, UP = 72, DOWN = 80 };
-
 int main()
 {
-
-	CONSOLE_FONT_INFOEX cfi;
-	cfi.cbSize = sizeof(cfi);
-	cfi.nFont = 0;
-	cfi.dwFontSize.X = 0;   
-	cfi.dwFontSize.Y = 72;            
-	cfi.FontFamily = FF_DONTCARE;
-	cfi.FontWeight = FW_NORMAL;
-	wcscpy(cfi.FaceName, L"Consolas"); 
-	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
-
-	system("title Maze");
-	MoveWindow(GetConsoleWindow(), 20, 60, 1850, 900, true);
-	// 50 - отступ слева от левой границы рабочего стола до левой границы окна консоли (в пикселях!)
-	// 60 - отступ сверху от верхней границы РС до верхней границы окна консоли
-	// 1700 - ширина окна консоли в пикселях
-	// 1000 - высота окна консоли
-
-	srand(time(0));
+	// enumeration (перечисление - это набор именованных целочисленных констант)
+	// MazeObject - пользовательский (кастомизированный) тип данных
+	enum MazeObject { HALL, WALL, COIN, ENEMY, BORDER };
+	enum Color { DARKGREEN = 2, YELLOW = 14, RED = 12, BLUE = 9, WHITE = 15, DARKYELLOW = 6, DARKRED = 4 };
+	enum KeyCode { ENTER = 13, ESCAPE = 27, SPACE = 32, LEFT = 75, RIGHT = 77, UP = 72, DOWN = 80 };
 
 	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	const int width = 40; // ширина лабиринта
-	const int height = 10; // высота лабиринта
+	system("color 07");
 
-	int maze[height][width]{}; // maze - лабиринт по английски
+	// настройки шрифта консоли
+	CONSOLE_FONT_INFOEX font; // https://docs.microsoft.com/en-us/windows/console/console-font-infoex
+	font.cbSize = sizeof(font);
+	font.dwFontSize.Y = 70;
+	font.FontFamily = FF_DONTCARE;
+	font.FontWeight = FW_NORMAL;
+	wcscpy_s(font.FaceName, 9, L"Consolas");
+	SetCurrentConsoleFontEx(h, 0, &font);
+
+	// скрытие мигающего курсора 
+	CONSOLE_CURSOR_INFO cursor;
+	cursor.bVisible = false; // спрятать курсор
+	cursor.dwSize = 1; // 1...100
+	SetConsoleCursorInfo(h, &cursor);
+
+	system("title Maze");
+	MoveWindow(GetConsoleWindow(), 20, 60, 1850, 900, true);
+	// 20 - отступ слева от левой границы рабочего стола до левой границы окна консоли (в пикселях!)
+	// 60 - отступ сверху от верхней границы рабочего стола до верхней границы окна консоли
+	// 1850 - ширина окна консоли в пикселях
+	// 900 - высота окна консоли
+	// true - перерисовать окно после перемещения
+
+	srand(time(0));
+
+	const int WIDTH = 40; // ширина лабиринта
+	const int HEIGHT = 10; // высота лабиринта
+
+	int maze[HEIGHT][WIDTH] = {}; // maze - лабиринт по-английски
 
 	// алгоритм заполнения массива
-	for (int y = 0; y < height; y++) // перебор строк
+	for (int y = 0; y < HEIGHT; y++) // перебор строк
 	{
-		for (int x = 0; x < width; x++) // перебор столбцов
+		for (int x = 0; x < WIDTH; x++) // перебор столбцов
 		{
 			maze[y][x] = rand() % 4; // 4 типа объектов в игре
 
-			if (maze[y][x] == MazeObjects::ENEMY) // если в лабиринте сгенерился враг
+			if (maze[y][x] == MazeObject::ENEMY) // если в лабиринте сгенерился враг
 			{
-				int probability = rand() % 10; // 0...4, если выпало 0 - враг останется 01234 останется только одна пятая часть врагов
+				int probability = rand() % 10; // 0...9, если выпало 0 - враг останется, останется только одна пятая часть врагов
 				if (probability != 0) // убираем врага
 				{
-					maze[y][x] = MazeObjects::HALL; // на место врага ставим коридор
+					maze[y][x] = MazeObject::HALL; // на место врага ставим коридор
 				}
 			}
 
-			if (maze[y][x] == MazeObjects::WALL) // если в лабиринте сгенерилась стена
+			if (maze[y][x] == MazeObject::WALL) // если в лабиринте сгенерировалась стена
 			{
-				int probability = rand() % 2; // 0...4, если выпало 0 - враг останется 01234 останется только одна пятая часть врагов
+				int probability = rand() % 2; // 0...1, если выпало 0 - стена останется, останется только половина стен
 				if (probability != 0) // убираем стену
 				{
-					maze[y][x] = MazeObjects::HALL; // на место стены ставим коридор
+					maze[y][x] = MazeObject::HALL; // на место стены ставим коридор
 				}
 			}
 
-			if (x == 0 || y == 0 || x == width - 1 || y == height - 1) maze[y][x] = MazeObjects::BORDER;
+			if (x == 0 || y == 0 || x == WIDTH - 1 || y == HEIGHT - 1) maze[y][x] = MazeObject::BORDER; // белая рамка
 
-			if (x == 0 && y == 2 || x == 1 && y == 2 || x == 2 && y == 2) maze[y][x] = MazeObjects::HALL; // вход
+			if (x == 0 && y == 2 || x == 1 && y == 2 || x == 2 && y == 2) maze[y][x] = MazeObject::HALL; // вход
 
-			if (x == width - 1 && y == height - 3 ||
-				x == width - 2 && y == height - 3 ||
-				x == width - 3 && y == height - 3) maze[y][x] = MazeObjects::HALL; // выход
+			if (x == WIDTH - 1 && y == HEIGHT - 3 ||
+				x == WIDTH - 2 && y == HEIGHT - 3 ||
+				x == WIDTH - 3 && y == HEIGHT - 3) maze[y][x] = MazeObject::HALL; // выход
 
 		}
 	}
 
 	// показ лабиринта
-	for (int y = 0; y < height; y++) // перебор строк
+	for (int y = 0; y < HEIGHT; y++) // перебор строк
 	{
-		for (int x = 0; x < width; x++) // перебор столбцов
+		for (int x = 0; x < WIDTH; x++) // перебор столбцов
 		{
 			switch (maze[y][x])
 			{
-
-			case MazeObjects::HALL: // hall - коридор
+			case MazeObject::HALL: // hall - коридор
 				cout << " ";
 				break;
 
-			case MazeObjects::WALL: // wall - стена
-				SetConsoleTextAttribute(h, Colors::DARKGREEN);
+			case MazeObject::WALL: // wall - стена
+				SetConsoleTextAttribute(h, Color::DARKGREEN);
 				cout << (char)178;
 				break;
 
-			case MazeObjects::BORDER: // border - рамка
-				SetConsoleTextAttribute(h, Colors::WHITE);
+			case MazeObject::BORDER: // border - рамка
+				SetConsoleTextAttribute(h, Color::WHITE);
 				cout << (char)178;
 				break;
 
-			case MazeObjects::COIN: // coin - монетка
-				SetConsoleTextAttribute(h, Colors::YELLOW);
+			case MazeObject::COIN: // coin - монетка
+				SetConsoleTextAttribute(h, Color::YELLOW);
 				cout << ".";
 				break;
 
-			case MazeObjects::ENEMY: // enemy - враг
-				SetConsoleTextAttribute(h, Colors::RED);
+			case MazeObject::ENEMY: // enemy - враг
+				SetConsoleTextAttribute(h, Color::RED);
 				cout << (char)1;
 				break;
 			}
@@ -114,19 +117,13 @@ int main()
 	}
 
 	/////////////////////////////////////////////////////////////////////
-	// скрытие мигающего курсора 
-	CONSOLE_CURSOR_INFO cursor;
-	cursor.bVisible = false; // спрятать курсор
-	cursor.dwSize = 1; // 1...100
-	SetConsoleCursorInfo(h, &cursor);
 
 	// размещение главного героя (ГГ)
 	COORD position; // координаты нашего персонажа
 	position.X = 0;
 	position.Y = 2;
 	SetConsoleCursorPosition(h, position);
-	//cout << "@";
-	SetConsoleTextAttribute(h, Colors::BLUE);
+	SetConsoleTextAttribute(h, Color::BLUE);
 	cout << (char)2;
 
 	int coins = 0; // счётчик собранных монет
@@ -135,20 +132,19 @@ int main()
 	/////////////////////////////////////////////////////////////////////
 	// информация по всем показателям
 	COORD infobox;
-	infobox.X = width + 1;
+	infobox.X = WIDTH + 1;
 	infobox.Y = 1;
 	SetConsoleCursorPosition(h, infobox);
-	SetConsoleTextAttribute(h, Colors::DARKYELLOW);
+	SetConsoleTextAttribute(h, Color::DARKYELLOW);
 	cout << "COINS: ";
-	SetConsoleTextAttribute(h, Colors::YELLOW);
+	SetConsoleTextAttribute(h, Color::YELLOW);
 	cout << coins << "\n"; // 0
 
-	//infobox.X = width + 1;
 	infobox.Y = 2;
 	SetConsoleCursorPosition(h, infobox);
-	SetConsoleTextAttribute(h, Colors::DARKRED);
+	SetConsoleTextAttribute(h, Color::DARKRED);
 	cout << "HEALTH: ";
-	SetConsoleTextAttribute(h, Colors::RED);
+	SetConsoleTextAttribute(h, Color::RED);
 	cout << health << "\n";
 
 	while (true)
@@ -164,104 +160,95 @@ int main()
 			SetConsoleCursorPosition(h, position);
 			cout << " ";
 
-			// cout << code << "\n"; // отладочный сиаут для других клавиш кроме перечисленных ниже
-
-			if (code == KeyCodes::ENTER)
+			if (code == KeyCode::ENTER)
 			{
 				cout << "ENTER!\n";
 				system("cls");
 				exit(0);
 			}
-			else if (code == KeyCodes::ESCAPE)
+			else if (code == KeyCode::ESCAPE)
 			{
 				cout << "ESCAPE!\n";
 				system("cls");
 				exit(0);
 			}
-			else if (code == KeyCodes::SPACE)
+			else if (code == KeyCode::SPACE)
 			{
 				cout << "SPACE!\n";
 				system("cls");
 				exit(0);
 			}
-			else if (code == KeyCodes::LEFT
-				&& maze[position.Y][position.X - 1] != MazeObjects::WALL
-				&& maze[position.Y][position.X - 1] != MazeObjects::BORDER)
+			else if (code == KeyCode::LEFT
+				&& maze[position.Y][position.X - 1] != MazeObject::WALL
+				&& maze[position.Y][position.X - 1] != MazeObject::BORDER)
 			{
-				//cout << "LEFT!\n";
 				position.X--;
 			}
-			else if (code == KeyCodes::RIGHT // если я собрался пойти направо
-				&& maze[position.Y][position.X + 1] != MazeObjects::WALL
-				&& maze[position.Y][position.X + 1] != MazeObjects::BORDER)
+			else if (code == KeyCode::RIGHT // если я собрался пойти направо
+				&& maze[position.Y][position.X + 1] != MazeObject::WALL
+				&& maze[position.Y][position.X + 1] != MazeObject::BORDER)
 				// и при этом в лабиринте на той же строке (где смайлик) и
 				// немного (на одну ячейку) правее на 1 столбец от смайлика
 			{
-				// cout << "RIGHT!\n";
 				position.X++;
 			}
-			else if (code == KeyCodes::UP
-				&& maze[position.Y - 1][position.X] != MazeObjects::WALL
-				&& maze[position.Y - 1][position.X] != MazeObjects::BORDER)
+			else if (code == KeyCode::UP
+				&& maze[position.Y - 1][position.X] != MazeObject::WALL
+				&& maze[position.Y - 1][position.X] != MazeObject::BORDER)
 			{
-				//cout << "UP!\n";
 				position.Y--;
 			}
-			else if (code == KeyCodes::DOWN
-				&& maze[position.Y + 1][position.X] != MazeObjects::WALL
-				&& maze[position.Y + 1][position.X] != MazeObjects::BORDER)
+			else if (code == KeyCode::DOWN
+				&& maze[position.Y + 1][position.X] != MazeObject::WALL
+				&& maze[position.Y + 1][position.X] != MazeObject::BORDER)
 			{
-				//cout << "DOWN!\n";
 				position.Y++;
 			}
 
 			// показ персонажика в новой позиции
 			SetConsoleCursorPosition(h, position);
-			SetConsoleTextAttribute(h, Colors::BLUE);
+			SetConsoleTextAttribute(h, Color::BLUE);
 			cout << (char)2;
 
 			////////////////////////////////////////////////////////////////
 			// пересечение с элементами массива
-			if (position.Y == height - 3 &&
-				position.X == width - 1)
+			if (position.Y == HEIGHT - 3 &&
+				position.X == WIDTH - 1)
 			{
 				MessageBoxA(0, "найден выход из лабиринта!", "ПОБЕДА!", 0);
 				system("cls");
 				exit(0);
-				//main(); // для запуска сначала но на другой рандомной локации
+				// main(); // для запуска сначала но на другой рандомной локации
 			}
 
 			// пересечение с монетками
 			// если в лабиринте по позиции смайлика (под ним) находится монетка
-			if (maze[position.Y][position.X] == MazeObjects::COIN)
+			if (maze[position.Y][position.X] == MazeObject::COIN)
 			{
 				coins++; // на одну монетку собрали больше
-				//cout << coins << "\n"; // показ количества
-				COORD info;
-				info.X = width + 1;
-				info.Y = 1;
-				SetConsoleCursorPosition(h, info);
-				SetConsoleTextAttribute(h, Colors::DARKYELLOW);
+				infobox.X = WIDTH + 1;
+				infobox.Y = 1;
+				SetConsoleCursorPosition(h, infobox);
+				SetConsoleTextAttribute(h, Color::DARKYELLOW);
 				cout << "COINS: ";
-				SetConsoleTextAttribute(h, Colors::YELLOW);
+				SetConsoleTextAttribute(h, Color::YELLOW);
 				cout << coins << "\n";
-				maze[position.Y][position.X] = MazeObjects::HALL; // убираем монетку из лабиринта
+				maze[position.Y][position.X] = MazeObject::HALL; // убираем монетку из лабиринта
 			}
 
 			// пересечение с врагами
 			// если в лабиринте по позиции смайлика (под ним) находится враг
-			if (maze[position.Y][position.X] == MazeObjects::ENEMY)
+			if (maze[position.Y][position.X] == MazeObject::ENEMY)
 			{
 				health -= 20;
-				COORD info;
-				info.X = width + 1;
-				info.Y = 2;
-				SetConsoleCursorPosition(h, info);
-				SetConsoleTextAttribute(h, Colors::DARKRED);
+				infobox.X = WIDTH + 1;
+				infobox.Y = 2;
+				SetConsoleCursorPosition(h, infobox);
+				SetConsoleTextAttribute(h, Color::DARKRED);
 				cout << "HEALTH: ";
-				SetConsoleTextAttribute(h, Colors::RED);
+				SetConsoleTextAttribute(h, Color::RED);
 				cout << health << " \n";
-				maze[position.Y][position.X] = MazeObjects::HALL; // убираем монетку из лабиринта
+				maze[position.Y][position.X] = MazeObject::HALL; // убираем монетку из лабиринта
 
 				if (health <= 0)
 				{
@@ -275,8 +262,6 @@ int main()
 					{
 						exit(0);
 					}
-
-
 				}
 			}
 		}
@@ -289,12 +274,12 @@ int main()
 			int enemy_count = 0; // реальное количество врагов на данный момент после шага ГГ
 
 			// перебор всех ячеек которые есть в лабиринте
-			for (int y = 0; y < height; y++) // перебор строк
+			for (int y = 0; y < HEIGHT; y++) // перебор строк
 			{
-				for (int x = 0; x < width; x++) // перебор столбцов
+				for (int x = 0; x < WIDTH; x++) // перебор столбцов
 				{
 					// если очередная ячейка - это враг
-					if (maze[y][x] == MazeObjects::ENEMY)
+					if (maze[y][x] == MazeObject::ENEMY)
 					{
 						enemy_positions[enemy_count].X = x;
 						enemy_positions[enemy_count].Y = y;
@@ -303,99 +288,88 @@ int main()
 				}
 			}
 
-			// cout << enemy_count << "\n";
-
 			// перебор всех врагов
 			for (int i = 0; i < enemy_count; i++)
 			{
-				int r = rand() % 100; // 0-left 1-right 2-up 3-down
-				if (r == 0 &&
-					maze[enemy_positions[i].Y][enemy_positions[i].X - 1] != MazeObjects::WALL &&
-					maze[enemy_positions[i].Y][enemy_positions[i].X - 1] != MazeObjects::BORDER &&
-					maze[enemy_positions[i].Y][enemy_positions[i].X - 1] != MazeObjects::ENEMY) // left
+				int r = rand() % 100; // 0-left, 1-right, 2-up, 3-down
+				if (r == LEFT &&
+					maze[enemy_positions[i].Y][enemy_positions[i].X - 1] != MazeObject::WALL &&
+					maze[enemy_positions[i].Y][enemy_positions[i].X - 1] != MazeObject::BORDER &&
+					maze[enemy_positions[i].Y][enemy_positions[i].X - 1] != MazeObject::ENEMY)
 				{
 					// стирание врага в старой позиции
-					COORD temp;
-					temp.X = enemy_positions[i].X;
-					temp.Y = enemy_positions[i].Y;
+					COORD temp = enemy_positions[i];
 					SetConsoleCursorPosition(h, temp);
 					cout << " ";
-					maze[enemy_positions[i].Y][enemy_positions[i].X] = MazeObjects::HALL;
+					maze[enemy_positions[i].Y][enemy_positions[i].X] = MazeObject::HALL;
 
 					// перемещаем врага в новую позицию
 					temp.X = enemy_positions[i].X - 1;
 					temp.Y = enemy_positions[i].Y;
 					SetConsoleCursorPosition(h, temp);
-					SetConsoleTextAttribute(h, Colors::RED);
+					SetConsoleTextAttribute(h, Color::RED);
 					cout << (char)1;
-					maze[enemy_positions[i].Y][enemy_positions[i].X - 1] = MazeObjects::ENEMY;
+					maze[enemy_positions[i].Y][enemy_positions[i].X - 1] = MazeObject::ENEMY;
 				}
-				else if (r == 1 && // right
-					maze[enemy_positions[i].Y][enemy_positions[i].X + 1] != MazeObjects::WALL &&
-					maze[enemy_positions[i].Y][enemy_positions[i].X + 1] != MazeObjects::BORDER &&
-					maze[enemy_positions[i].Y][enemy_positions[i].X + 1] != MazeObjects::ENEMY)
+				else if (r == RIGHT &&
+					maze[enemy_positions[i].Y][enemy_positions[i].X + 1] != MazeObject::WALL &&
+					maze[enemy_positions[i].Y][enemy_positions[i].X + 1] != MazeObject::BORDER &&
+					maze[enemy_positions[i].Y][enemy_positions[i].X + 1] != MazeObject::ENEMY)
 				{
 					// стирание врага в старой позиции
-					COORD temp;
-					temp.X = enemy_positions[i].X;
-					temp.Y = enemy_positions[i].Y;
+					COORD temp = enemy_positions[i];
 					SetConsoleCursorPosition(h, temp);
 					cout << " ";
-					maze[enemy_positions[i].Y][enemy_positions[i].X] = MazeObjects::HALL;
+					maze[enemy_positions[i].Y][enemy_positions[i].X] = MazeObject::HALL;
 
 					// перемещаем врага в новую позицию
 					temp.X = enemy_positions[i].X + 1;
 					temp.Y = enemy_positions[i].Y;
 					SetConsoleCursorPosition(h, temp);
-					SetConsoleTextAttribute(h, Colors::RED);
+					SetConsoleTextAttribute(h, Color::RED);
 					cout << (char)1;
-					maze[enemy_positions[i].Y][enemy_positions[i].X + 1] = MazeObjects::ENEMY;
+					maze[enemy_positions[i].Y][enemy_positions[i].X + 1] = MazeObject::ENEMY;
 				}
 
-				else if (r == 2 && // up
-					maze[enemy_positions[i].Y - 1][enemy_positions[i].X] != MazeObjects::WALL &&
-					maze[enemy_positions[i].Y - 1][enemy_positions[i].X] != MazeObjects::BORDER &&
-					maze[enemy_positions[i].Y - 1][enemy_positions[i].X] != MazeObjects::ENEMY)
+				else if (r == UP &&
+					maze[enemy_positions[i].Y - 1][enemy_positions[i].X] != MazeObject::WALL &&
+					maze[enemy_positions[i].Y - 1][enemy_positions[i].X] != MazeObject::BORDER &&
+					maze[enemy_positions[i].Y - 1][enemy_positions[i].X] != MazeObject::ENEMY)
 				{
 					// стирание врага в старой позиции
-					COORD temp;
-					temp.X = enemy_positions[i].X;
-					temp.Y = enemy_positions[i].Y;
+					COORD temp = enemy_positions[i];
 					SetConsoleCursorPosition(h, temp);
 					cout << " ";
-					maze[enemy_positions[i].Y][enemy_positions[i].X] = MazeObjects::HALL;
+					maze[enemy_positions[i].Y][enemy_positions[i].X] = MazeObject::HALL;
 
 					// перемещаем врага в новую позицию
 					temp.X = enemy_positions[i].X;
 					temp.Y = enemy_positions[i].Y - 1;
 					SetConsoleCursorPosition(h, temp);
-					SetConsoleTextAttribute(h, Colors::RED);
+					SetConsoleTextAttribute(h, Color::RED);
 					cout << (char)1;
-					maze[enemy_positions[i].Y - 1][enemy_positions[i].X] = MazeObjects::ENEMY;
+					maze[enemy_positions[i].Y - 1][enemy_positions[i].X] = MazeObject::ENEMY;
 				}
-				else if (r == 3 && // down
-					maze[enemy_positions[i].Y + 1][enemy_positions[i].X] != MazeObjects::WALL &&
-					maze[enemy_positions[i].Y + 1][enemy_positions[i].X] != MazeObjects::BORDER &&
-					maze[enemy_positions[i].Y + 1][enemy_positions[i].X] != MazeObjects::ENEMY)
+				else if (r == DOWN &&
+					maze[enemy_positions[i].Y + 1][enemy_positions[i].X] != MazeObject::WALL &&
+					maze[enemy_positions[i].Y + 1][enemy_positions[i].X] != MazeObject::BORDER &&
+					maze[enemy_positions[i].Y + 1][enemy_positions[i].X] != MazeObject::ENEMY)
 				{
 					// стирание врага в старой позиции
-					COORD temp;
-					temp.X = enemy_positions[i].X;
-					temp.Y = enemy_positions[i].Y;
+					COORD temp = enemy_positions[i];
 					SetConsoleCursorPosition(h, temp);
 					cout << " ";
-					maze[enemy_positions[i].Y][enemy_positions[i].X] = MazeObjects::HALL;
+					maze[enemy_positions[i].Y][enemy_positions[i].X] = MazeObject::HALL;
 
 					// перемещаем врага в новую позицию
 					temp.X = enemy_positions[i].X;
 					temp.Y = enemy_positions[i].Y + 1;
 					SetConsoleCursorPosition(h, temp);
-					SetConsoleTextAttribute(h, Colors::RED);
+					SetConsoleTextAttribute(h, Color::RED);
 					cout << (char)1;
-					maze[enemy_positions[i].Y + 1][enemy_positions[i].X] = MazeObjects::ENEMY;
+					maze[enemy_positions[i].Y + 1][enemy_positions[i].X] = MazeObject::ENEMY;
 				}
 			}
 		}
 	}
-
 }
